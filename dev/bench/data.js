f3ed8b4ee4f3de366aafb75a66e92426fb6f3ed8
github-action-benchmark <github@users.noreply.github.com>
2020-10-16 01:54:58 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 313a941e0b4aa78dc9ec77b5bc2fa70fdbf54d62

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602813298273,
+  "repoUrl": "https://github.com/CherylTo/benchmark-test",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "CherylTo",
+            "username": "CherylTo"
+          },
+          "committer": {
+            "name": "CherylTo",
+            "username": "CherylTo"
+          },
+          "id": "313a941e0b4aa78dc9ec77b5bc2fa70fdbf54d62",
+          "message": "Create main.yml",
+          "timestamp": "2020-10-16T01:51:50Z",
+          "url": "https://github.com/CherylTo/benchmark-test/pull/1/commits/313a941e0b4aa78dc9ec77b5bc2fa70fdbf54d62"
+        },
+        "date": 1602813297646,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_bench",
+            "value": 5681042.385120725,
+            "unit": "iter/sec",
+            "range": "stddev: 2.3313265557406908e-7",
+            "extra": "mean: 176.0240343599493 nsec\nrounds: 192308"
+          }
+        ]
+      }
+    ]
+  }
+}
