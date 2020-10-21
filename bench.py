6d81e7d0dc168f748cdff438caedfe1130261d05
Cheryl To <>
2020-10-20 20:03:11 -0400

    update bench.py

--- bench.py
+++ bench.py
@@ -1,10 +1,18 @@
 import pytest
+import json
 
-def add():
-    a = 1
-    b = 2
+def add(a, b):
     return a+b
 
-def test_bench(benchmark):
-    res = benchmark(add)
-    assert res == 3
+def multiply(a, b):
+    return a*b
+
+def test_bench():
+    sum = add(1, 2)
+    product = multiply(2, 3)
+    data = {
+        "add": [{"a": 1, "b": 2, "add": sum}],
+	"multiply": [{"a": 2, "b": 3, "multiply": product}]
+    }
+    with open('test.json', 'w') as file:
+        json.dump(data, file)
