05cd8d0275d86fa479ca3cee494ddcea3c6fe921
Cheryl To <>
2020-10-20 21:28:05 -0400

    update bench'

--- bench.py
+++ bench.py
@@ -11,10 +11,12 @@ def multiply(a, b):
 def test_bench():
     sum = add(1, 2)
     product = multiply(2, 3)
+    ts = str(int(time.time()))
     data = {
+        "time": time.time
         "add": [{"a": 1, "b": 2, "add": sum}],
-	"multiply": [{"a": 2, "b": 3, "multiply": product}]
+	    "multiply": [{"a": 2, "b": 3, "multiply": product}]
     }
-    file_name = str(int(time.time()))+'_test.json'
+    file_name = ts+'_test.json'
     with open(file_name, 'w') as file:
         json.dump(data, file)
